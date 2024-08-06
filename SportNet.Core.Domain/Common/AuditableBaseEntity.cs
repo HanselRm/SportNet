80762fb113c386ac8b77cca69680e2833420e13f
HanselRm <20220897@itla.edu.do>
2024-08-06 10:18:52 -0400

    Common property

---
+++ SportNet.Core.Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace SportNet.Core.Domain.Common
+{
+    public class AuditableBaseEntity
+    {
+        public virtual int Id { get; set; }
+        public string? CreateBy { get; set; }
+        public DateTime? Created { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
