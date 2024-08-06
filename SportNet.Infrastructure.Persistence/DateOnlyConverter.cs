14db7e25641ea4087d49f4dc20d3ddb32ac98827
HanselRm <20220897@itla.edu.do>
2024-08-06 11:01:40 -0400

    conversor de fecha

---
+++ SportNet.Infrastructure.Persistence/DateOnlyConverter.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace SportNet.Infrastructure.Persistence
+{
+    //conversor de fecha a la hora de guardar en la base de datos
+    public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
+    {
+        public DateOnlyConverter() : base(
+                dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue),
+                dateTime => DateOnly.FromDateTime(dateTime))
+        {
+        }
+    }
+
+    public class DateOnlyComparer : ValueComparer<DateOnly>
+    {
+        public DateOnlyComparer() : base(
+            (d1, d2) => d1.DayNumber == d2.DayNumber,
+            d => d.GetHashCode())
+        {
+        }
+    }
+}
