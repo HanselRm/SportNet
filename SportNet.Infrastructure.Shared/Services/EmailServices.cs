68a79f79a887f34636bcdf0a606aea6cbab404e8
HanselRm <20220897@itla.edu.do>
2024-08-06 19:26:26 -0400

    Servicio de Email

--- SportNet.Infrastructure.Shared/Services/EmailServices.cs
+++ SportNet.Infrastructure.Shared/Services/EmailServices.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MailKit.Security;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using SportNet.Core.Application.DTOS.Email;
+using SportNet.Core.Application.Services;
+using SportNet.Core.Domain.Settings;
+
 
 namespace SportNet.Infrastructure.Shared.Services
 {
-    internal class EmailServices
+    public class EmailServices : IEmailServices
     {
+        private MailSettings _mailSettings { get; }
+        public EmailServices(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+        public async Task sendAsync(EmailRequest request)
+        {
+
+            try
+            {
+                MimeMessage email = new();
+                email.Sender = MailboxAddress.Parse(_mailSettings.EmailFrom);
+                email.To.Add(MailboxAddress.Parse(request.To));
+                email.Subject = request.Subject;
+                BodyBuilder builder = new BodyBuilder();
+                builder.HtmlBody = request.Body;
+                email.Body = builder.ToMessageBody();
+
+
+                using SmtpClient smtp = new();
+                smtp.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                smtp.Connect(_mailSettings.SmtpHost, _mailSettings.SmtpPort, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.SmtpUser, _mailSettings.SmtpPass);
+                await smtp.SendAsync(email);
+                smtp.Disconnect(true);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
