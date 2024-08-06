b47288b07b9fefa545c43e3f2ecca5a974edccf2
HanselRm <20220897@itla.edu.do>
2024-08-06 11:00:23 -0400

    Conversion de hora

---
+++ SportNet.Infrastructure.Persistence/TimeOnlyConverter.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace SportNet.Infrastructure.Persistence
+{
+    //clase para convertir la hora cuando se guarde en la base de datos
+    public class TimeOnlyConverter : ValueConverter<TimeOnly, TimeSpan>
+    {
+        public TimeOnlyConverter() : base(
+                timeOnly => timeOnly.ToTimeSpan(),
+                timeSpan => TimeOnly.FromTimeSpan(timeSpan))
+        {
+        }
+    }
+
+    public class TimeOnlyComparer : ValueComparer<TimeOnly>
+    {
+        public TimeOnlyComparer() : base(
+            (t1, t2) => t1.Ticks == t2.Ticks,
+            t => t.GetHashCode())
+        {
+        }
+    }
+}
+}
