4fec3ae91a96b0e4a0ce8f0c4b8236a55f5d6f8a
HanselRm <20220897@itla.edu.do>
2024-08-06 19:04:02 -0400

    Logica de repositorio generico

---
+++ SportNet.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportNet.Core.Application.Interfaces.Repositories;
+using SportNet.Infrastructure.Persistence.Context;
+
+
+namespace SportNet.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<Entity> : IGenericRepository<Entity>
+        where Entity : class
+    {
+        private readonly DbContex dbContext;
+
+        public GenericRepository(DbContex dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public virtual async Task<Entity> AddAsync(Entity entity)
+        {
+            await dbContext.Set<Entity>().AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+        public virtual async Task UdapteAsync(Entity entity, int id)
+        {
+            Entity entry = await dbContext.Set<Entity>().FindAsync(id);
+            dbContext.Entry(entry).CurrentValues.SetValues(entity);
+            await dbContext.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(Entity entity)
+        {
+            dbContext.Set<Entity>().Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+        public async Task<List<Entity>> GetAllAsync()
+        {
+            return await dbContext.Set<Entity>().ToListAsync();
+        }
+
+        public async Task<List<Entity>> GetAllWithIncludeAsync(List<string> properties)
+        {
+            var query = dbContext.Set<Entity>().AsQueryable();
+            foreach (var property in properties)
+            {
+                query = query.Include(property);
+            }
+            return await query.ToListAsync();
+        }
+        public async Task<Entity> GetByIdAsync(int id)
+        {
+            return await dbContext.Set<Entity>().FindAsync(id);
+        }
+    }
+}
