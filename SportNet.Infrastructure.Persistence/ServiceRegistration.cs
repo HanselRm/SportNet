00c20f06e7154c459a7e24084187794b7496f64e
HanselRm <20220897@itla.edu.do>
2024-08-06 10:52:59 -0400

    Dependencias

---
+++ SportNet.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SportNet.Infrastructure.Persistence.Context;
+
+namespace SportNet.Infrastructure.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceInfrastructure(this IServiceCollection service, IConfiguration configuracion)
+        {
+            #region Context
+            service.AddDbContext<DbContex>(option =>
+                option.UseSqlServer(configuracion.GetConnectionString("Default"),
+                m => m.MigrationsAssembly(typeof(DbContex).Assembly.FullName)));
+            #endregion
+
+            return service;
+        }
+    }
+}
--- SportNet/Program.cs
+++ SportNet/Program.cs
@@ -1,7 +1,16 @@
+using SportNet.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//usar sesiones
+builder.Services.AddSession();
+
+//Dependencias
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddPersistenceInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -12,6 +21,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
