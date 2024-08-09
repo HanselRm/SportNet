99bddecf00ec8f687faee5c2653bfb5e9f10365e
HanselRm <20220897@itla.edu.do>
2024-08-08 20:55:19 -0400

    Event repository

---
+++ SportNet.Infrastructure.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,18 @@
+﻿
+
+using SportNet.Core.Application.Interfaces.Repositories;
+using SportNet.Core.Domain.Entities;
+using SportNet.Infrastructure.Persistence.Context;
+
+namespace SportNet.Infrastructure.Persistence.Repositories
+{
+    public class EventRepository : GenericRepository<Events>, IEventRepository
+    {
+        private readonly DbContex dbContext;
+
+        public EventRepository(DbContex dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
